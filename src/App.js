8003624b1b8e388d6608834048cfd7fc5ea78ff8
Erik <erik.ondra@yahoo.com>
2023-06-01 18:15:51 +0200

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import StartComponent from "./components/StartComponent";
 import ParticleBackground from "./components/ParticleBackground";
 import "./App.css";
+import AboutMeComponent from "./components/AboutMeComponent";
 
 const App = () => {
   return (
     <div className="App">
       <ParticleBackground />
       <StartComponent />
+      <AboutMeComponent />
     </div>
   );
 };
--- src/components/StartComponent.jsx
+++ src/components/StartComponent.jsx
@@ -3,11 +3,15 @@ import React from "react";
 const StartComponent = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen w-screen bg-gray-800">
-      <div className="text-white text-3xl text-center sm:text-3xl md:text-4xl lg:text-5xl">
-        Hello, I'm <span className="text-rose-500">Erik Ondra</span>.<br /> I'm
-        a full-stack developer.
+      <div className="z-[1] text-white text-3xl text-center sm:text-3xl md:text-4xl lg:text-5xl">
+        Hello, I'm{" "}
+        <span className="text-emerald-500 font-black">Erik Ondra</span>.
+        <br /> I'm a full-stack developer.
       </div>
-      <button className="bg-transparent text-white hover:text-gray-800 py-2 px-4 border border-white mt-10 text-xl hover:bg-white transition-colors duration-300">
+      <button
+        className=" z-[1] bg-transparent text-white hover:text-gray-800 py-2 px-4 border
+       border-white mt-10 text-xl hover:bg-emerald-500 transition-colors duration-300 hover:border-emerald-500"
+      >
         View my work
       </button>
     </div>
